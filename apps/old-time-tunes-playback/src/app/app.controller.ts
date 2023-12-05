270734160480cc29103946c94e56bcdee75824d9
David <25187726+david-shortman@users.noreply.github.com>
2023-12-05 18:49:52 -0500

    format all files

--- apps/old-time-tunes-playback/src/app/app.controller.ts
+++ apps/old-time-tunes-playback/src/app/app.controller.ts
@@ -1,9 +1,25 @@
-import {Controller, Get, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { AppService } from './app.service';
-import {FileInterceptor} from "@nestjs/platform-express";
-import {combineLatest, debounceTime, from, map, Observable, reduce, Subject, switchMap, take} from "rxjs";
+import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  combineLatest,
+  debounceTime,
+  from,
+  map,
+  Observable,
+  reduce,
+  Subject,
+  switchMap,
+  take,
+} from 'rxjs';
 import { Express } from 'express';
 // This is a hack to make Multer available in the Express namespace
 import { Multer } from 'multer';
@@ -12,10 +28,10 @@ import {
   BasicPitch,
   NoteEventTime,
   noteFramesToTime,
-  outputToNotesPoly
-} from "@spotify/basic-pitch";
+  outputToNotesPoly,
+} from '@spotify/basic-pitch';
 import * as tf from '@tensorflow/tfjs-node';
-import { decoders} from 'audio-decode';
+import { decoders } from 'audio-decode';
 
 @Controller()
 export class AppController {
@@ -28,8 +44,10 @@ export class AppController {
 
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File): Observable<Array<NoteEventTime>> {
-       console.log(file);
+  uploadFile(
+    @UploadedFile() file: Express.Multer.File
+  ): Observable<Array<NoteEventTime>> {
+    console.log(file);
 
     const audioData = from(decoders.mp3(file.buffer));
 
@@ -38,41 +56,48 @@ export class AppController {
     const basicPitch = new BasicPitch(model as any);
 
     const evaluations = new Subject<{
-      frames: Array<number[]>,
-      onsets: Array<number[]>,
-      contours: Array<number[]>,
+      frames: Array<number[]>;
+      onsets: Array<number[]>;
+      contours: Array<number[]>;
     }>();
 
     const evaluated = evaluations.pipe(
-      reduce((acc, {frames, onsets, contours}) => ({
-        frames: acc.frames.concat(frames),
-        onsets: acc.onsets.concat(onsets),
-        contours: acc.contours.concat(contours),
-      }), {
-        frames: [],
-        onsets: [],
-        contours: [],
-      })
+      reduce(
+        (acc, { frames, onsets, contours }) => ({
+          frames: acc.frames.concat(frames),
+          onsets: acc.onsets.concat(onsets),
+          contours: acc.contours.concat(contours),
+        }),
+        {
+          frames: [],
+          onsets: [],
+          contours: [],
+        }
+      )
     );
 
     const evaluate = audioData.pipe(
-      map(audioBuffer => audioBuffer.getChannelData(0)),
-      switchMap(audioBuffer=> from(basicPitch.evaluateModel(
-        audioBuffer,
-          (frames, onsets, contours) => {
-             console.log('frames', frames);
-          evaluations.next({frames, onsets, contours});
-          },
-          (percent: number) => {
-          console.log(percent);
-          if (percent === 1) evaluations.complete();
-          },
-        ))
-      ));
+      map((audioBuffer) => audioBuffer.getChannelData(0)),
+      switchMap((audioBuffer) =>
+        from(
+          basicPitch.evaluateModel(
+            audioBuffer,
+            (frames, onsets, contours) => {
+              console.log('frames', frames);
+              evaluations.next({ frames, onsets, contours });
+            },
+            (percent: number) => {
+              console.log(percent);
+              if (percent === 1) evaluations.complete();
+            }
+          )
+        )
+      )
+    );
 
     return combineLatest([evaluate, evaluated]).pipe(
       take(1),
-      map(([_, {frames, onsets, contours}]) => {
+      map(([_, { frames, onsets, contours }]) => {
         const notes = noteFramesToTime(
           addPitchBendsToNoteEvents(
             contours,
@@ -86,19 +111,32 @@ export class AppController {
             melodiaTrick?: boolean,
             energyTolerance?: number
              */
-            outputToNotesPoly(frames, onsets, 0.25, 0.25, 5),
-          ),
+            outputToNotesPoly(frames, onsets, 0.25, 0.25, 5)
+          )
         );
         console.log(notes);
-        return notes.map(note => ({
+        return notes.map((note) => ({
           ...note,
           noteName: this.midiNumberToNoteName(note.pitchMidi),
         }));
-      }),
+      })
     );
   }
   private midiNumberToNoteName(midiNumber: number) {
-    const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+    const notes = [
+      'C',
+      'C#',
+      'D',
+      'D#',
+      'E',
+      'F',
+      'F#',
+      'G',
+      'G#',
+      'A',
+      'A#',
+      'B',
+    ];
     const octave = Math.floor(midiNumber / 12) - 1;
     const noteIndex = midiNumber % 12;
     return notes[noteIndex] + octave;
